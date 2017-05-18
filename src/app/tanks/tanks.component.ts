ec96fe750d0c01ac482894f9bec0aed8462236d6
scottvang07 <scottvang07@gmail.com>
2017-05-18 16:06:15 -0700

    tank page wired up

--- src/app/tanks/tanks.component.ts
+++ src/app/tanks/tanks.component.ts
@@ -13,16 +13,36 @@ export class TanksComponent implements OnInit {
   tanks:Array<Object>;
 
   constructor(
+    private tankService: TankService,
     private router: Router
   ) { 
   }
 
   ngOnInit() {
     this.tanks = [];
+    this.getTanks();
+    console.log('tanks', this.tanks);
+  }
+
+  getTanks() {
+     this.tankService.getTanks().then((resp) => {
+      this.tanks = resp;
+     });  
   }
 
   goToCreate() {
     this.router.navigate(['tank-create']);
+  }
+
+  deleteUser(id:string) {
+    console.log(`deleting tank with id of : ${id}`);
+    this.tankService.deleteTank(id).then((resp) => {
+      if(resp) {
+        this.tanks = this.tanks.filter((tank) => {
+          return tank['id'] != id;
+        });
+      }
+    });
   }
 
 }
